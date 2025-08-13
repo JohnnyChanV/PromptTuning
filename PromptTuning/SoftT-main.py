df05e45df3963aff530e66f89f06803ac3eefa66
JohnnyChanV <13602337187@163.com>
2025-08-13 17:07:18 -0400

    good

--- PromptTuning/SoftT-main.py
+++ PromptTuning/SoftT-main.py
@@ -309,7 +309,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--model_name", type=str, default="meta-llama/Llama-3.1-8B-Instruct")
     parser.add_argument("--train_data", type=str, default="../RAG_data/proc_dev_data.json")
     parser.add_argument("--train_dimension_filter", type=str, default=None)
-    parser.add_argument("--train_size", type=int, default=100)
+    parser.add_argument("--train_size", type=int, default=5000)
     # choices = ['Organization', 'Explanations', 'Textual.Evidence', 'Rhetorical.Strategies', 'nan', 'Argument', 'Thesis', 'Language']
     parser.add_argument("--resample_train", action="store_true", default=True)
     parser.add_argument("--system_prompt_file", type=str, default="good_prompt.txt")
