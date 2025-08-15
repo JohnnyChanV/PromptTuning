8cd646743686081903aa7e68457777c6c42fbe3c
JohnnyChanV <13602337187@163.com>
2025-08-15 17:46:22 -0400

    good

--- PromptTuning/run.sh
+++ PromptTuning/run.sh
@@ -2,19 +2,19 @@
 
 
 python SoftT-main.py \
-  --exp_name "Llama-3.2-3B-100-E.T.R"\
+  --exp_name "Llama-3.2-3B-E.T.R"\
   --train_dimension_filter "['nan','Explanations', 'Textual.Evidence', 'Rhetorical.Strategies']" \
   --resample_train
 
 
 python SoftT-main.py \
-  --exp_name "Llama-3.2-3B-100-A.T"\
+  --exp_name "Llama-3.2-3B-A.T"\
   --train_dimension_filter "['nan', 'Argument', 'Thesis']" \
   --resample_train
 
 
 python SoftT-main.py \
-  --exp_name "Llama-3.2-3B-100-O.L"\
+  --exp_name "Llama-3.2-3B-O.L"\
   --train_dimension_filter "['nan','Organization', 'Language']" \
   --resample_train
 
