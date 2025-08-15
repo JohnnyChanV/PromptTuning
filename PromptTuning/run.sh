cfa56d7d5a112e17b677b82595bc83bb8a2df2ae
JohnnyChanV <13602337187@163.com>
2025-08-15 16:28:10 -0400

    good

--- PromptTuning/run.sh
+++ PromptTuning/run.sh
@@ -3,35 +3,14 @@
 
 python SoftT-main.py \
   --exp_name "Llama-3.2-3B-100-exclude-Organization"\
-  --train_dimension_filter "['Explanations', 'Textual.Evidence', 'Rhetorical.Strategies', 'nan', 'Argument', 'Thesis', 'Language']" \
+  --train_dimension_filter "['nan','Explanations', 'Textual.Evidence', 'Rhetorical.Strategies']" \
 
 
 python SoftT-main.py \
   --exp_name "Llama-3.2-3B-100-exclude-Explanations"\
-  --train_dimension_filter "['Organization', 'Textual.Evidence', 'Rhetorical.Strategies', 'nan', 'Argument', 'Thesis', 'Language']" \
+  --train_dimension_filter "['nan', 'Argument', 'Thesis']" \
 
 
 python SoftT-main.py \
   --exp_name "Llama-3.2-3B-100-exclude-TE"\
-  --train_dimension_filter "['Organization', 'Explanations', 'Rhetorical.Strategies', 'nan', 'Argument', 'Thesis', 'Language']" \
-
-
-python SoftT-main.py \
-  --exp_name "Llama-3.2-3B-100-exclude-RS"\
-  --train_dimension_filter "['Organization', 'Explanations', 'Textual.Evidence', 'nan', 'Argument', 'Thesis', 'Language']" \
-
-
-python SoftT-main.py \
-  --exp_name "Llama-3.2-3B-100-exclude-Argument"\
-  --train_dimension_filter "['Organization', 'Explanations', 'Textual.Evidence', 'nan', 'Thesis', 'Language']" \
-
-
-python SoftT-main.py \
-  --exp_name "Llama-3.2-3B-100-exclude-Thesis"\
-  --train_dimension_filter "['Organization', 'Explanations', 'Textual.Evidence', 'nan', 'Argument', 'Language']" \
-
-
-python SoftT-main.py \
-  --exp_name "Llama-3.2-3B-100-exclude-Language"\
-  --train_dimension_filter "['Organization', 'Explanations', 'Textual.Evidence', 'nan', 'Argument', 'Thesis']" \
-
+  --train_dimension_filter "['nan','Organization', 'Language']" \
