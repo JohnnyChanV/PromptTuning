f3b85f4fe9a27478d25b982b4fbc88d2e1115950
JohnnyChanV <13602337187@163.com>
2025-08-15 17:25:25 -0400

    good

--- PromptTuning/run.sh
+++ PromptTuning/run.sh
@@ -2,27 +2,27 @@
 
 
 python SoftT-main.py \
-  --exp_name "Llama-3.2-3B-100-exclude-Organization"\
+  --exp_name "Llama-3.2-3B-100-E.T.R"\
   --train_dimension_filter "['nan','Explanations', 'Textual.Evidence', 'Rhetorical.Strategies']" \
   --resample_train
 
 
 python SoftT-main.py \
-  --exp_name "Llama-3.2-3B-100-exclude-Explanations"\
+  --exp_name "Llama-3.2-3B-100-A.T"\
   --train_dimension_filter "['nan', 'Argument', 'Thesis']" \
   --resample_train
 
 
 python SoftT-main.py \
-  --exp_name "Llama-3.2-3B-100-exclude-TE"\
+  --exp_name "Llama-3.2-3B-100-O.L"\
   --train_dimension_filter "['nan','Organization', 'Language']" \
   --resample_train
 
 
 
 
-python SoftT-main.py \
-  --model_name "meta-llama/Llama-3.1-70B-Instruct"\
-  --exp_name "Llama-3-70B-all"\
-  --train_size 20000 \
-  --gradient_checkpointing
+#python SoftT-main.py \
+#  --model_name "meta-llama/Llama-3.1-70B-Instruct"\
+#  --exp_name "Llama-3-70B-all"\
+#  --train_size 20000 \
+#  --gradient_checkpointing
