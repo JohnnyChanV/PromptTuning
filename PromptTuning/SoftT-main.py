3ddf261d67b4e19605905fbd4b429351bcfd910c
JohnnyChanV <13602337187@163.com>
2025-08-21 14:14:13 -0400

    good

--- PromptTuning/SoftT-main.py
+++ PromptTuning/SoftT-main.py
@@ -403,7 +403,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--learning_rate", type=float, default=5e-5)
     parser.add_argument("--batch_size", type=int, default=4)
     parser.add_argument("--warmup_ratio", type=float, default=0)
-    parser.add_argument("--weight_decay", type=float, default=0.05)
+    parser.add_argument("--weight_decay", type=float, default=0.01)
     parser.add_argument("--epochs", type=int, default=5)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--exp_name", type=str, default="Llama-3.2-3B-100-domain")
--- PromptTuning/run.sh
+++ PromptTuning/run.sh
@@ -34,28 +34,26 @@ git pull
 #-----------train with trainsize
 
 python SoftT-main.py \
-  --model_name "meta-llama/Llama-3.2-3B-Instruct"\
-  --exp_name "Llama-3B-ETR-600"\
+  --model_name "meta-llama/Llama-3.1-8B-Instruct"\
+  --exp_name "Llama-8B-ETR-1000"\
   --train_dimension_filter "['nan','Explanations', 'Textual.Evidence', 'Rhetorical.Strategies']" \
-  --resample_train \
-  --train_size 600
+  --resample_train\
+  --train_size 1000
 
 python SoftT-main.py \
-  --model_name "meta-llama/Llama-3.2-3B-Instruct"\
-  --exp_name "Llama-3B-AT-500"\
+  --model_name "meta-llama/Llama-3.1-8B-Instruct"\
+  --exp_name "Llama-8B-AT-400"\
   --train_dimension_filter "['nan', 'Argument', 'Thesis']" \
   --resample_train\
-  --train_size 500 \
-  --batch_size 16
+  --train_size 1000
 
 
 python SoftT-main.py \
-  --model_name "meta-llama/Llama-3.2-3B-Instruct"\
-  --exp_name "Llama-3B-OL-500"\
+  --model_name "meta-llama/Llama-3.1-8B-Instruct"\
+  --exp_name "Llama-8B-OL-400"\
   --train_dimension_filter "['nan','Organization', 'Language']" \
   --resample_train\
-  --train_size 500 \
-  --batch_size 16
+  --train_size 1000
 
 
 
