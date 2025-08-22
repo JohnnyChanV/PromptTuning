61ed72f0fe60e99481d8987f39c2cd9a5e941743
JohnnyChanV <13602337187@163.com>
2025-08-22 11:24:49 -0400

    good

---
+++ data_proc/process.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import json
+from sklearn.model_selection import train_test_split
+
+data = pd.read_excel("data_proc/highschoolAPEng_Comment_data.xlsx",sheet_name="coded for explanation")
+
+data['input'] = data['Comment']
+data['label'] = data['Explanation (human code)']
+data['Dimension.Name'] = data['Dimension.Name'].apply(lambda x:x.lower())
+
+records = data.to_dict('records')
+
+dev,test = train_test_split(records,test_size=14228,random_state=42)
+
+from collections import Counter
+Counter([item['Dimension.Name'] for item in test])
+Counter([item['Dimension.Name'] for item in dev])
+
+from collections import Counter
+
+Counter([item['Dimension.Name'] for item in records])
+
+for key in test[0].keys():
+    print(type(test[0][key]))
+
+json.dump(test,open("data_proc/proc_test_data.json",'w'),ensure_ascii=False,default=str)
+json.dump(test,open("data_proc/proc_dev_data.json",'w'),ensure_ascii=False,default=str)
