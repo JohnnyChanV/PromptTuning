8cbc06587d979675b87dad00c70b7fe8890ce8b9
JohnnyChanV <13602337187@163.com>
2025-08-22 20:06:15 -0400

    good

--- PromptTuning/run.sh
+++ PromptTuning/run.sh
@@ -39,7 +39,8 @@ python SoftT-main.py \
   --train_dimension_filter "['Explanations', 'Textual.Evidence', 'Rhetorical.Strategies']" \
   --resample_train\
   --train_size 500\
-  --batch_size 4
+  --batch_size 4\
+  --epochs 10
 
 python SoftT-main.py \
   --model_name "meta-llama/Llama-3.1-8B-Instruct"\
@@ -47,7 +48,8 @@ python SoftT-main.py \
   --train_dimension_filter "['Argument', 'Thesis']" \
   --resample_train\
   --train_size 500\
-  --batch_size 4
+  --batch_size 4\
+  --epochs 10
 
 
 python SoftT-main.py \
@@ -56,7 +58,8 @@ python SoftT-main.py \
   --train_dimension_filter "['Organization', 'Language']" \
   --resample_train\
   --train_size 500\
-  --batch_size 4
+  --batch_size 4\
+  --epochs 10
 
 
 
